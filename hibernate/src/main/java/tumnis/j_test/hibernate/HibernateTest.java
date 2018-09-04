e2ede07250ba6089ff83fe34fa811ce9df4222cd
Sergei Tumanov <tumanser@gmail.com>
2018-09-04 22:40:39 +0300

    Init HibernateTest project

---
+++ hibernate/src/main/java/tumnis/j_test/hibernate/HibernateTest.java
@@ -0,0 +1,14 @@
+package tumnis.j_test.hibernate;
+
+
+
+/**
+ * Hello world!
+ */
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hibernate Test");
+	}
+
+}
---
+++ hibernate/src/test/java/tumnis/j_test/hibernate/HibernateTestTest.java
@@ -0,0 +1,23 @@
+package tumnis.j_test.hibernate;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+/**
+ * Unit test for App.
+ */
+public class HibernateTestTest {
+
+	/**
+	 * App test.
+	 */
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+	}
+
+}
