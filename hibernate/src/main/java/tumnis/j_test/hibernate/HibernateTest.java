b24f6a95d1007d32850813e517404207876ad58f
Sergei Tumanov <tumanser@gmail.com>
2018-09-05 00:19:15 +0300

    Working with im-memory H2 DB

--- hibernate/src/main/java/tumnis/j_test/hibernate/HibernateTest.java
+++ hibernate/src/main/java/tumnis/j_test/hibernate/HibernateTest.java
@@ -2,13 +2,91 @@ package tumnis.j_test.hibernate;
 
 
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import tumnis.j_test.hibernate.entity.OrderEntity;
+
+
+
 /**
- * Hello world!
+ * Hibernate Test
  */
 public class HibernateTest {
 
+	private static final EntityManagerFactory entityManagerFactory;
+	static {
+		try {
+			entityManagerFactory = Persistence.createEntityManagerFactory("test");
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	private static EntityManager getEntityManager() {
+		return entityManagerFactory.createEntityManager();
+	}
+
+	private static EntityManager entityManager = getEntityManager();
+
+
+
 	public static void main(String[] args) {
 		System.out.println("Hibernate Test");
+		createData();
+		listData();
+	}
+
+
+
+	private static void createData() {
+		runInTransaction(() -> {
+
+			OrderEntity orderEntity;
+			OrderEntity orderEntity1;
+
+			orderEntity = new OrderEntity("T1", "T1-P");
+			entityManager.persist(orderEntity);
+
+			orderEntity1 = new OrderEntity("T1", "T1-C1");
+			orderEntity1.setParent(orderEntity);
+			entityManager.persist(orderEntity1);
+
+		});
+	}
+
+	private static void listData() {
+		runInTransaction(() -> {
+
+			List<OrderEntity> orders = entityManager.createQuery("from OrderEntity").getResultList();
+			for (OrderEntity order: orders) {
+				System.out.printf("order: %s\n", order);
+			}
+
+		});
+	}
+
+
+
+	private interface TransactionalExec {
+		void run() throws Exception;
+	}
+
+	private static void runInTransaction(TransactionalExec exec) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		try {
+			transaction.begin();
+			exec.run();
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ hibernate/src/main/java/tumnis/j_test/hibernate/entity/OrderEntity.java
@@ -0,0 +1,89 @@
+package tumnis.j_test.hibernate.entity;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "\"order\"")
+public class OrderEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column
+	private String type;
+
+	@Column
+	private String name;
+
+	@ManyToOne(targetEntity=OrderEntity.class, fetch=FetchType.LAZY, optional=true)
+	@JoinColumn(name="parent")
+	private OrderEntity parent;
+
+	@Column
+	private int customer;
+
+
+
+	public OrderEntity() {
+	}
+
+	public OrderEntity(String type, String name) {
+		this.type = type;
+		this.name = name;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return String.format("{ id: %d, type: '%s', name: '%s', parent: %s }", id, type, name, parent);
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public OrderEntity getParent() {
+		return parent;
+	}
+
+	public void setParent(OrderEntity parent) {
+		this.parent = parent;
+	}
+
+	public int getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(int customer) {
+		this.customer = customer;
+	}
+
+}
